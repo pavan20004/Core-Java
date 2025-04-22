3a4992296a571670a2933852b3fa14bf5785bc0f
pavan <pavan@gmail.com>
2025-04-22 12:48:24 +0530

    commit

---
+++ InBuild_Functions_In_Java/src/StringMethods/StringMethods.java
@@ -0,0 +1,33 @@
+package StringMethods;
+
+public class StringMethods {
+	
+	public static void main(String[] args) {
+		
+		/*
+		 String Methods :
+		 concat();
+		 substring();
+		 charAt()
+		 contains()
+		 compareTo()
+		 compareToIgnoreCase()
+		 equals()
+		 equalsIgnoreCase()
+		 index()
+		 replace()
+		 replaceFirst()
+		 split()
+		 startsWith()
+		 endsWith()
+		 toLowerCase()
+		 toUpperCase()
+		 trim()
+		 isEmpty()
+		 String.valueOf()
+		 join()
+		 
+		 		 */
+	}
+
+}
